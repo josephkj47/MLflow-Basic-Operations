7d94da7855b1997ae4928e8832c8827c0b879372
josephkj47 <josephkj47@gmail.com>
2024-08-21 02:35:23 -0400

    Dagshub installation

--- example.py
+++ example.py
@@ -1,3 +1,13 @@
+import dagshub
+dagshub.init(repo_owner='josephkj47',
+             repo_name='MLflow-Basic-Operations',
+             mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
+  
 import logging
 import sys
 import warnings
